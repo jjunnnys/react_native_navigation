f739572f8cdeffdda9bc09e4adb98515443550b3
ppby <salswnss@gmail.com>
2020-09-23 16:00:48 +0900

    [Drawer] Style 설정 01

--- App.js
+++ App.js
@@ -1,67 +1,58 @@
 import 'react-native-gesture-handler'; // react navigation
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, Button} from 'react-native';
+import {StyleSheet, Text, View, Image, Button, Linking} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'; // 네비게이션 상태와 구조 관리
 import {createStackNavigator} from '@react-navigation/stack';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 
 import Home from './src/Home';
 import User from './src/User';
 import LogoTile from './src/Logo';
+import HomeDrawer from './src/HomeDrawer';
+import UserDrawer from './src/UserDrawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Help"
+        onPress={() => Linking.openURL('https://www.google.com')}
+      />
+      <DrawerItem label="Info" onPress={() => alert('Info Window')} />
+    </DrawerContentScrollView>
+  );
+};
 
 class App extends Component {
-  logoTitle = () => {
-    return (
-      <Image
-        style={{width: 30, height: 30}}
-        source={require('./src/assets/pics/home_icon.png')}
-      />
-    );
-  };
-
   render() {
     return (
+      // headerbar 가 사라진다.
       <NavigationContainer>
-        <Stack.Navigator
+        <Drawer.Navigator
           initialRouteName="Home"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#dedede',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              color: 'gray',
-            },
-          }}>
-          <Stack.Screen
-            name="Home" // home 루트
-            component={Home}
-            options={{
-              title: 'Home Screen',
-              // headerTitle: <this.logoTitle />,
-              headerTitle: <LogoTile />,
-              headerBackTitleVisible: false,
-              headerRight: () => (
-                <Button
-                  title="Info"
-                  onPress={() => alert('I am Button')}
-                  color="red"
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="User" // user 루트
-            component={User}
-            initialParams={{
-              userIdx: 40,
-              userName: 'Minjun',
-              userLastName: 'Seo',
-            }}
-          />
-        </Stack.Navigator>
+          drawerType="front"
+          drawerPosition="right"
+          drawerStyle={{
+            backgroundColor: '#c6cbef',
+            width: 200,
+          }}
+          drawerContentOptions={{
+            activeTintColor: 'red',
+            activeBackgroundColor: 'skyblue',
+          }}
+          drawerContent={(props) => <CustomDrawerContent {...props} />}>
+          <Drawer.Screen name="Home" component={HomeDrawer} />
+          <Drawer.Screen name="User" component={UserDrawer} />
+        </Drawer.Navigator>
       </NavigationContainer>
     );
   }
---
+++ src/UserDrawer.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Button, Text, View} from 'react-native';
+
+class UserDrawer extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text> User </Text>
+        <Button
+          title="To Home Screen"
+          onPress={() => {
+            this.props.navigation.navigate('Home'); // 스크린을 등록하면 props로 넘겨 받는다. (name값을 인자로 입력)
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+export default UserDrawer;
